ab3fb27808caf44f5cee0b26e80a5c03acd749d4
duyguakin <duyguakin509@gmail.com>
2021-10-22 14:28:36 +0300

    22 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,27 @@
+console.log("merhaba Kodlama.io")
+
+
+let dolarBugun = 9.30 
+
+let dolarDun = 9.40
+ 
+
+
+console.log(dolarDun)
+
+
+const euroDun = 11.10   
+
+
+console.log(euroDun)
+
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+console.log("<ul>")
+for(let i = 0; i<konutKredileri.length; i++) {
+  console.log("<li>"+konutKredileri[i]+"</li>")   
+}
+console.log("</ul>")
+
+
+console.log(konutKredileri)
